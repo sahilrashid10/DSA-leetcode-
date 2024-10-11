f06526c7a004fc657aa5dcfbce17a6b662978e69
SAHIL RASHID <srashid_be22@thapar.edu>
2024-10-11 19:53:33 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (87.17%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,23 +1,46 @@
  class Solution {
 public:
     int myAtoi(string s) {
-        long ans = 0;
-        int sign = 1;
-        int i = 0;
+//  ROUND 1
+    //     long ans = 0;
+    //     int sign = 1;
+    //     int i = 0;
 
-        while (s[i] == ' ') i++;
+    //     while (s[i] == ' ') i++;
 
-        if (s[i] == '-' || s[i] == '+') {
-            sign = (s[i] == '-') ? -1 : 1;
-            i++;
+    //     if (s[i] == '-' || s[i] == '+') {
+    //         sign = (s[i] == '-') ? -1 : 1;
+    //         i++;
+    //     }
+    //     while (i < s.length() && isdigit(s[i])) {
+    //         ans = ans * 10 + (s[i] - '0');
+    //         if (ans*sign <= INT_MIN) return INT_MIN;
+    //         if (ans*sign >= INT_MAX) return INT_MAX;
+    //         i++;
+    //     }
+    //     return ans * sign;
+// ROUND 2 (brute force)
+    int n = s.size();
+    int ans = 0;    // ans long lele sol optimal hojaiga bcz INT_MAX/10 NAI SEEDHA ans >= INT_MAX \U0001f636 
+    int sign = 1;
+    int i = 0;
+    while(s[i]==' ')
+        i++;
+    if(s[i] == '-'||s[i]=='+'){
+        sign= (s[i]=='-')?-1:1;
+        i++;            
         }
-        while (i < s.length() && isdigit(s[i])) {
-            ans = ans * 10 + (s[i] - '0');
-            if (ans * sign <= INT_MIN) return INT_MIN;
-            if (ans * sign >= INT_MAX) return INT_MAX;
-            i++;
+    while(s[i]=='0')
+        i++;
+    while(i<n && s[i]>='0' && s[i]<='9'){
+        if(ans > INT_MAX/10 || (ans == INT_MAX/10 && s[i]>'7')){
+            return (sign == 1)?INT_MAX:INT_MIN;
+            }
+        else
+            ans = ans*10 + (s[i]-'0');
+        i++;
         }
+    return ans*sign;
 
-        return ans * sign;
     }
 };
