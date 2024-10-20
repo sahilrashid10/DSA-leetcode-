2fdd9a04112d23ee6d360a6dfa4ef5fee71ceef1
SAHIL RASHID <srashid_be22@thapar.edu>
2024-10-20 13:16:53 +0530

    Update 0078-subsets.cpp

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -14,3 +14,26 @@ public:
         return power_set;
     }
 };
+
+// -------------------using recursion--------------------------------------------
+class Solution{
+public:
+    void solve(vector<vector<int>>&p, vector<int>s, vector<int>&nums, int i, int n){
+        //base case 
+        if(i == n){
+            p.push_back(s);
+            return;
+        }
+        s.push_back(nums[i]);
+        solve(p, s, nums, i+1, n);
+        s.pop_back();
+        solve(p,s, nums, i+1, n);
+    }
+    vector<vector<int>> subsets(vector<int>& nums){
+        vector<vector<int>>power_set;
+        int n = nums.size();
+        vector<int>sub_set;
+        solve(power_set, sub_set, nums, 0, n);
+        return power_set;
+    }
+};
