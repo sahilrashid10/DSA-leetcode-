13b06fe0f0dd5fc00a265957360b87430d88d470
SAHIL RASHID <srashid_be22@thapar.edu>
2024-11-05 11:18:03 +0530

    Time: 283 ms (69.27%), Space: 9.8 MB (86.07%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-int m;
-int n;
-    vector<vector<int>>directions = {{0,1}, {0,-1}, {1,0},{-1,0}};
-    bool FindWord(vector<vector<char>>&board,string &word ,int i, int j, int len){
-        if(len == word.length()) return true;
-
-        if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='$')return false;
-
-        if(word[len] != board[i][j]) return false;
-
-        char ch = board[i][j];
-        board[i][j] ='$';
-        for(auto &dir: directions){
-            int new_i = i+dir[0];
-            int new_j = j+dir[1];
-            if(FindWord(board, word, new_i, new_j, len+1))return true;
+    int n, m;
+    vector<pair<int, int>>dirn = {{0,1},{0,-1},{1,0},{-1,0}};
+    bool solve(vector<vector<char>>&board, string &word, int idx, int i, int j){
+        if(idx == word.size()) return true;
+        if(i<0 || j<0 || i>=n || j>=m || board[i][j] != word[idx] || board[i][j]=='$')
+            return false;
+        
+        char temp = board[i][j];
+        board[i][j] = '$';
+        for(auto it:dirn){
+            int new_i = i + it.first;
+            int new_j = j + it.second;
+            if(solve(board, word, idx+1, new_i, new_j)){
+                return true;
+                }
         }
-        board[i][j] = ch;
+        board[i][j] = temp;
         return false;
+
     }
-    bool exist(vector<vector<char>>& board, string word) {
-        m = board.size(); 
-        n = board[0].size();
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(board[i][j] == word[0] && FindWord(board,word ,i, j, 0))
+
+    bool exist(vector<vector<char>>& board, string word){
+        n= board.size();
+        m = board[0].size();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j]==word[0] && solve(board, word, 0, i, j))
                     return true;
             }
         }
