9f6cfa822179431c58493c1f51731c0615d632b9
SAHIL RASHID <srashid_be22@thapar.edu>
2024-11-09 10:47:57 +0530

    Time: 0 ms (100.00%), Space: 30.3 MB (23.44%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,18 +1,32 @@
+// class Solution {
+// public:
+//     int ans=-1;
+//     void solve(vector<int>& nums, int k, int l, int r){
+//         if(l>r) return;
+//         int m = (l+r)/2;
+//         if(nums[m]==k){
+//             ans = m;
+//             return;
+//             }
+//         (nums[m]<k)?solve(nums, k,m+1 ,r):solve(nums, k, l,m-1);
+//     }
+//     int search(vector<int>& nums, int target) {
+//         int n = nums.size()-1;
+//         solve(nums, target, 0, n);
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int ans=-1;
-    void solve(vector<int>& nums, int k, int l, int r){
-        if(l>r) return;
-        int m = (l+r)/2;
-        if(nums[m]==k){
-            ans = m;
-            return;
-            }
-        (nums[m]<k)?solve(nums, k,m+1 ,r):solve(nums, k, l,m-1);
-    }
     int search(vector<int>& nums, int target) {
-        int n = nums.size()-1;
-        solve(nums, target, 0, n);
-        return ans;
+    int l=0;
+    int r = nums.size()-1;
+    while(l<=r){
+        int m = (l+r)/2;
+        if(nums[m]==target){ans = m; break;}
+        (nums[m]<target)?(l=m+1):(r=m-1);
+    }
+    return ans;
     }
 };
