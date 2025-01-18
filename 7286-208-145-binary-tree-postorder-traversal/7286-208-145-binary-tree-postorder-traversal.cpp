cd70a0438c154485bd0b8a8bac1ae2281ae33696
SAHIL RASHID <srashid_be22@thapar.edu>
2025-01-18 17:37:23 +0530

    Time: 0 ms, Memory: 11 MB - LeetHub

--- 7286-208-145-binary-tree-postorder-traversal/7286-208-145-binary-tree-postorder-traversal.cpp
+++ 7286-208-145-binary-tree-postorder-traversal/7286-208-145-binary-tree-postorder-traversal.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //USING ONE STACK
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
@@ -16,24 +17,56 @@ public:
         if(!root)
             return ans;
         stack<TreeNode*>s;
-        stack<int>s1;
-        s.push(root);
-        while(!s.empty()){
-            TreeNode* curr = s.top();
-            s.pop();
-            s1.push(curr->val);
-            if(curr->left)
-                s.push(curr->left);
-            if(curr->right)
-                s.push(curr->right);
+        TreeNode* curr = root;
+        while(curr != NULL || !s.empty()){
+            if(curr != NULL){
+                s.push(curr);
+                curr = curr->left;
+            }else{
+                TreeNode* temp = s.top()->right;
+                if(temp == NULL){
+                    temp = s.top();
+                    s.pop();
+                    ans.push_back(temp->val);
+                    while(!s.empty() && s.top()->right == temp){
+                        temp = s.top();
+                        s.pop();
+                        ans.push_back(temp->val);
+                    }
+                }else{
+                    curr = temp;
+                }
             }
-            while(!s1.empty()){
-                ans.push_back(s1.top());
-                s1.pop();
-            }
+        }
         return ans;
     }
 };
+//USING 2 STACKS
+// class Solution {
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int>ans;
+//         if(!root)
+//             return ans;
+//         stack<TreeNode*>s;
+//         stack<int>s1;
+//         s.push(root);
+//         while(!s.empty()){
+//             TreeNode* curr = s.top();
+//             s.pop();
+//             s1.push(curr->val);
+//             if(curr->left)
+//                 s.push(curr->left);
+//             if(curr->right)
+//                 s.push(curr->right);
+//             }
+//             while(!s1.empty()){
+//                 ans.push_back(s1.top());
+//                 s1.pop();
+//             }
+//         return ans;
+//     }
+// };
 // class Solution {
 // public:
 //     vector<int>ans;
