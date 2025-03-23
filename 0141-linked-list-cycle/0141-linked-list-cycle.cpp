aa549c9d697b64a867c237b7c120d1fc618da7b2
SAHIL RASHID <srashid_be22@thapar.edu>
2025-03-23 09:26:03 +0530

    Time: 12 ms (39.85%), Space: 11.9 MB (23.52%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,27 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_set<ListNode*> s;
-        ListNode *temp = head;
-        while(temp){
-            if(s.find(temp) == s.end()){
-                s.insert(temp);
-                temp = temp->next;
-            }else return true;
+        ListNode* slow = head, *fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) return true;
         }
         return false;
     }
 };
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         unordered_set<ListNode*> s;
+//         ListNode *temp = head;
+//         while(temp){
+//             if(s.find(temp) == s.end()){
+//                 s.insert(temp);
+//                 temp = temp->next;
+//             }else return true;
+//         }
+//         return false;
+//     }
+// };
