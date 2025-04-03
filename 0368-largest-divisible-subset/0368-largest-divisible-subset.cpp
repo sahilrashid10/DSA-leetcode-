bb61a12fc8f80a022b4c8b7409e5720e3013a4d0
SAHIL RASHID <srashid_be22@thapar.edu>
2025-04-03 22:58:52 +0530

    Time: 9 ms (79.78%), Space: 12.7 MB (20.4%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -0,0 +1,71 @@
+//BOTTOM UP
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+
+        int n = nums.size();
+        vector<int> t(n, 1);
+
+        vector<int> prev_idx(n, -1);
+        int last_chosen_index = 0;
+        int maxL = 1;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i]%nums[j] == 0) {
+                    if(t[i] < t[j]+1) {
+                        t[i] = t[j]+1;
+                        prev_idx[i] = j;
+                    }
+
+                    if(t[i] > maxL) {
+                        maxL = t[i];
+                        last_chosen_index = i;
+                    }
+                }
+            }
+        }
+
+        vector<int> result;
+        while(last_chosen_index >= 0) {
+            result.push_back(nums[last_chosen_index]);
+            last_chosen_index = prev_idx[last_chosen_index];
+        }
+
+        return result;
+    }
+};
+
+//RECURSION- no memoization , bcz dp would be hugeee asf
+// class Solution {
+// public:
+//     int n;
+//     vector<int> result, temp;
+//     void solve(vector<int>& nums, int p, int i){
+//         if(i >= n){
+//             if(temp.size() > result.size())
+//                 result = temp;
+//             return;
+//         }
+//             if(p == -1 || nums[i] % nums[p] == 0){
+//                 temp.push_back(nums[i]);
+//                 solve(nums, i, i+1);
+//                 if(temp.size() > 0)
+//                     temp.pop_back();
+//             }
+
+//             solve(nums, p, i+1);
+
+//     }
+    
+//     vector<int> largestDivisibleSubset(vector<int>& nums) {
+//         n = nums.size();
+//         if(n == 1) return {nums[0]};
+
+//         sort(nums.begin(), nums.end());
+
+//         solve(nums, -1, 0);
+//         return result;
+//     }
+// };
