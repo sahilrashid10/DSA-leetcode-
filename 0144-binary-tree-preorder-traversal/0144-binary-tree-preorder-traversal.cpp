b1ed2cef8eb427f93de42b861884dc48e1fc56e4
SAHIL RASHID <srashid_be22@thapar.edu>
2025-07-06 23:33:33 +0530

    Time: 0 ms (100%), Space: 11 MB (14.52%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,19 +9,56 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ //Morris Traversal
 class Solution {
 public:
-    void preOrder(TreeNode* root, vector<int>& ans){
-        if(!root) return;
-
-        ans.push_back(root->val);
-        preOrder(root->left, ans);
-        preOrder(root->right, ans);
-    }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        preOrder(root, ans);
+        vector<int> preOrder;
+        if(root && !root->left && !root->right)
+            return {root->val};
 
-        return ans;
+        while(root){
+            if(root->left){
+                TreeNode* temp = root->left;
+
+                while(temp->right && temp->right != root){
+                    temp = temp->right;
+                }
+                if(temp->right == root){
+                    temp->right = nullptr;
+                    root = root->right;
+                }else{
+                    preOrder.push_back(root->val);
+                    temp->right = root;
+                    root = root->left;
+                }
+            }else{
+                preOrder.push_back(root->val);
+                root = root->right;
+            }
+        }
+        return preOrder;
     }
 };
+
+
+
+//Recursion
+
+// class Solution {
+// public:
+//     void preOrder(TreeNode* root, vector<int>& ans){
+//         if(!root) return;
+
+//         ans.push_back(root->val);
+//         preOrder(root->left, ans);
+//         preOrder(root->right, ans);
+//     }
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         preOrder(root, ans);
+
+//         return ans;
+//     }
+// };
