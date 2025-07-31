627df3d9cda4b96b9c2319d323eea89a2f8f2e3e
SAHIL RASHID <srashid_be22@thapar.edu>
2025-07-31 20:14:48 +0530

    Time: 0 ms (100%), Space: 17.3 MB (75.16%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,24 +1,34 @@
 class Solution {
+private: 
+    bool dfs(int node, int col, vector<int>& color, vector<vector<int>>& adj) {
+        color[node] = col; 
+        
+        // Traverse adjacent nodes
+        for(int it : adj[node]) {
+            // If uncolored
+            if(color[it] == -1) {
+                if(!dfs(it, !col, color, adj)) return false; 
+            }
+            // If previously colored and have the same color
+            else if(color[it] == col) {
+                return false; 
+            }
+        }
+        return true; 
+    }
+
 public:
-
-    bool dfs(vector<vector<int>>& graph,vector<int> &visited, int i, int cur){
-        visited[i] = cur;
-
-        for(auto &node : graph[i]){
-            if(visited[node] == -1 && !dfs(graph, visited, node, !cur)){
-                return false;
-            }else if(cur == visited[node])
-                return false;
+    bool isBipartite(vector<vector<int>>& adj) {
+        int V = adj.size();
+        vector<int> color(V, -1); // Initialize color vector with -1
+	    
+        // Check for all connected components
+        for(int i = 0; i < V; i++) {
+            if(color[i] == -1) {
+                if(!dfs(i, 0, color, adj)) 
+                    return false; 
+            }
         }
-        return true;
+        return true; 
     }
-    bool isBipartite(vector<vector<int>>& graph) {
-        vector<int> visited(graph.size(), -1);
-        for(int i=0; i<graph.size(); i++){
-            if(visited[i] == -1 && !dfs(graph, visited, i, 0))
-                return false;
-        }
-        return true;
-    }
-
 };
