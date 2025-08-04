95ef5c719ed87093cad4f4d36cfbfbccef7b7cc4
SAHIL RASHID <srashid_be22@thapar.edu>
2025-08-04 12:23:44 +0530

    Time: 0 ms (100%), Space: 10.2 MB (74.29%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,28 +1,61 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string> st;
-        int i=0;
+        int n = s.size();
+        int i=0, left = 0, right = 0;
 
-        while(i<s.size() && s[i] == ' ') i++;
+        reverse(s.begin(), s.end());
+        while(i<n){  
 
-        string temp = "";
-        for(; i<s.size(); i++){
-            while(i< s.size() && s[i] != ' '){
-                temp += s[i];
+            while(i<n && s[i] != ' '){
+                s[right] = s[i];
                 i++;
+                right++;
             }
-            if(temp != "")
-                st.push(temp);
-
-            temp = "";
+            if(left < right){
+                reverse(s.begin() + left, s.begin() + right);
+                s[right] = ' ';
+                right++;
+                left = right;
+            }
+            i++;
         }
-        temp = "";
-        while(!st.empty()){
-            temp += st.top() + ' ';
-            st.pop();
-        }
-        s = temp.substr(0,temp.size()-1);
+        s = s.substr(0, right-1);
         return s;
     }
 };
+
+
+
+
+
+
+// NOT OPTIMAL
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         stack<string> st;
+//         int i=0;
+
+//         while(i<s.size() && s[i] == ' ') i++;
+
+//         string temp = "";
+//         for(; i<s.size(); i++){
+//             while(i< s.size() && s[i] != ' '){
+//                 temp += s[i];
+//                 i++;
+//             }
+//             if(temp != "")
+//                 st.push(temp);
+
+//             temp = "";
+//         }
+//         temp = "";
+//         while(!st.empty()){
+//             temp += st.top() + ' ';
+//             st.pop();
+//         }
+//         s = temp.substr(0,temp.size()-1);
+//         return s;
+//     }
+// };
