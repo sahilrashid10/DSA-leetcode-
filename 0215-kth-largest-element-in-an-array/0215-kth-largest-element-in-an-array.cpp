f92fd2dac5f839970ad98bf2f2ac58e854deb342
SAHIL RASHID <srashid_be22@thapar.edu>
2025-08-08 17:29:03 +0530

    Time: 2192 ms (5%), Space: 59.2 MB (93.03%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,17 +1,53 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        //min-heap
-        priority_queue<int, vector<int>, greater<int>> h;
-        int i=0;
-        while(i<nums.size()){
-            h.push(nums[i]);
+    int n;
 
-            if(h.size() > k) 
-                h.pop();
-            
-            i++;
+    int getPivotIdx(vector<int>& nums, int left, int right){
+        int pivot = nums[right];
+        int Pi = left;
+          while(left < right){
+            if(nums[left] > pivot){
+                swap(nums[left], nums[Pi]);
+                Pi++;
+            }
+            left++;
         }
-        return h.top();
+        swap(nums[Pi], nums[right]);
+
+        return Pi;
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        n = nums.size();
+
+        int left = 0, right = n-1;
+        k--;
+        while(true){
+            int Pi = getPivotIdx(nums, left, right);
+            if(Pi > k)
+                right = Pi - 1;
+            else if(Pi < k)
+                left = Pi + 1;
+            else return nums[Pi];
+         }
+        return 0;
     }
 };
+
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         //min-heap
+//         priority_queue<int, vector<int>, greater<int>> h;
+//         int i=0;
+//         while(i<nums.size()){
+//             h.push(nums[i]);
+
+//             if(h.size() > k) 
+//                 h.pop();
+            
+//             i++;
+//         }
+//         return h.top();
+//     }
+// };
