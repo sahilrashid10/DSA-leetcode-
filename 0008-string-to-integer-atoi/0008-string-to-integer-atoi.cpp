7cc2cd596bf0848fec7fa6e89a2ec04052435928
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-08-16 12:25:51 +0530

    Time: 0 ms (100%), Space: 9.1 MB (82.02%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,46 +1,29 @@
- class Solution {
+class Solution {
 public:
     int myAtoi(string s) {
-//  ROUND 1
-    //     long ans = 0;
-    //     int sign = 1;
-    //     int i = 0;
+        int n = s.size(), i = 0, sign = 1, ans = 0;
 
-    //     while (s[i] == ' ') i++;
+        // skip leading spaces
+        while(i < n && s[i] == ' ') i++;
 
-    //     if (s[i] == '-' || s[i] == '+') {
-    //         sign = (s[i] == '-') ? -1 : 1;
-    //         i++;
-    //     }
-    //     while (i < s.length() && isdigit(s[i])) {
-    //         ans = ans * 10 + (s[i] - '0');
-    //         if (ans*sign <= INT_MIN) return INT_MIN;
-    //         if (ans*sign >= INT_MAX) return INT_MAX;
-    //         i++;
-    //     }
-    //     return ans * sign;
-// ROUND 2 (brute force)
-    int n = s.size();
-    int ans = 0;    // ans long lele sol optimal hojaiga bcz INT_MAX/10 NAI SEEDHA ans >= INT_MAX \U0001f636 
-    int sign = 1;
-    int i = 0;
-    while(s[i]==' ')
-        i++;
-    if(s[i] == '-'||s[i]=='+'){
-        sign= (s[i]=='-')?-1:1;
-        i++;            
+        // handle sign
+        if(i < n && s[i] == '-') { sign = -1; i++; }
+        else if(i < n && s[i] == '+') i++;
+
+        // skip leading zeros
+        while(i < n && s[i] == '0') i++;
+
+        // parse digits
+        while(i < n && s[i] >= '0' && s[i] <= '9') {
+            int digit = s[i] - '0';
+            // check overflow
+            if(ans > (INT_MAX - digit) / 10)
+                return sign == 1 ? INT_MAX : INT_MIN;
+
+            ans = ans * 10 + digit;
+            i++;
         }
-    while(s[i]=='0')
-        i++;
-    while(i<n && s[i]>='0' && s[i]<='9'){
-        if(ans > INT_MAX/10 || (ans == INT_MAX/10 && s[i]>'7')){
-            return (sign == 1)?INT_MAX:INT_MIN;
-            }
-        else
-            ans = ans*10 + (s[i]-'0');
-        i++;
-        }
-    return ans*sign;
 
+        return ans * sign;
     }
 };
