10fc8da426638010d142d060dafa2d23a8428cd7
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-08-19 12:20:43 +0530

    Time: 3 ms (51.85%), Space: 10 MB (11.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,36 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
+        if(s.size() != t.size()) return 0;
 
-        vector<int> f(26, 0); 
+        vector<int> a1(26, 0), a2(26, 0);
 
         for(int i=0; i<s.size(); i++){
-            f[s[i] - 'a']++;
-            f[t[i] - 'a']--;
+            a1[s[i]-'a']++;
+            a2[t[i]-'a']++;
         }
-        for(int i=0; i<26; i++){
-            if(f[i] != 0) return false;
-        }
-        return true;
+        return a1 == a2;
     }
 };
-
-// class Solution {
-// public:
-//     bool isAnagram(string s, string t) {
-//         if(s.size() != t.size()) return false;
-
-//         vector<int> f1(26, 0); 
-//         vector<int> f2(26, 0); 
-
-//         for(int i=0; i<s.size(); i++){
-//             f1[s[i] - 'a']++;
-//             f2[t[i] - 'a']++;
-//         }
-//         for(int i=0; i<26; i++){
-//             if(f1[i] != f2[i]) return false;
-//         }
-//         return true;
-//     }
-// };
