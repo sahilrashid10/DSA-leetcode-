f6e66e63c93db5f29b2e63c331a3afc2f2c01def
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-08-21 10:22:57 +0530

    Time: 0 ms (100%), Space: 8.1 MB (97.46%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -5,50 +5,22 @@ public:
         int v2 = version2.size();
 
         int i1=0, i2=0;
-        while(i1 < v1 && i2 < v2){
-            string s1 = "", s2="";
+        while(i1 < v1 || i2 < v2){
+            int n1 = 0, n2=0;
 
             while(i1<v1 && version1[i1] != '.'){ 
-                s1 += version1[i1];
+                n1 = n1*10 + (version1[i1] - '0');
                 i1++;
             }
             while(i2<v2 && version2[i2] != '.'){ 
-                s2 += version2[i2];
+                n2 = n2*10 + (version2[i2] - '0');
                 i2++;
             }
-            if(stoi(s1) < stoi(s2)) return -1;
+            if(n1 < n2) return -1;
 
-            else if(stoi(s1) > stoi(s2)) return 1;
-
-            i1++;
-            i2++;
-        }
-
-        
-        while(i1<v1){
-
-            string s1 = "";
-            while(i1<v1 && version1[i1] != '.'){ 
-                s1 += version1[i1];
-                i1++;
-            }
-            if(stoi(s1) < 0) return -1;
-
-            else if(stoi(s1) > 0) return 1;
+            else if(n1 > n2) return 1;
 
             i1++;
-        }
-        while(i2<v2){
-
-            string s2 = "";
-            while(i2<v2 && version2[i2] != '.'){ 
-                s2 += version2[i2];
-                i2++;
-            }
-            if(stoi(s2) > 0) return -1;
-
-            else if(stoi(s2) < 0) return 1;
-
             i2++;
         }
 
