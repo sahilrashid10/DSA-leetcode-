26e3719cb209a58bb036660cbcd6969b74635400
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-08-24 16:33:20 +0530

    Time: 3 ms (25.68%), Space: 60.1 MB (59.08%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,42 +1,62 @@
 class Solution {
 public:
-    int n;
-
-    void getPs(vector<int>& nums, vector<int>& v) {
-        stack<int> s;
-        for (int i = 0; i < n; i++) {
-            while (!s.empty() && nums[s.top()] > nums[i])
-                s.pop();
-            v[i] = s.empty() ? -1 : s.top();
-            s.push(i);
-        }
-    }
-
-    void getNs(vector<int>& nums, vector<int>& v) {
-        stack<int> s;
-        for (int i = n - 1; i >= 0; i--) {
-            while (!s.empty() && nums[s.top()] > nums[i])
-                s.pop();
-            v[i] = s.empty() ? n : s.top();
-            s.push(i);
-        }
-    }
-
     int longestSubarray(vector<int>& nums) {
-        n = nums.size();
-        vector<int> ps(n), ns(n);
+        int n = nums.size();
+        int i = 0, zeros = 0, maxLen = 0;
 
-        getPs(nums, ps);
-        getNs(nums, ns);
+        for (int j = 0; j < n; j++) {
+            if (nums[j] == 0) zeros++;
 
-        int maxLen = 0, z = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 1) continue;
-            z++;
-            int len = ns[i] - ps[i] - 2;
-            maxLen = max(maxLen, len);
+            while (zeros > 1) {
+                if (nums[i] == 0) zeros--;
+                i++;
+            }
+            maxLen = max(maxLen, j - i);  // delete one element, so (j - i)
         }
-        if (z == 0) return n - 1; 
         return maxLen;
     }
 };
+
+
+// class Solution {
+// public:
+//     int n;
+
+//     void getPs(vector<int>& nums, vector<int>& v) {
+//         stack<int> s;
+//         for (int i = 0; i < n; i++) {
+//             while (!s.empty() && nums[s.top()] > nums[i])
+//                 s.pop();
+//             v[i] = s.empty() ? -1 : s.top();
+//             s.push(i);
+//         }
+//     }
+
+//     void getNs(vector<int>& nums, vector<int>& v) {
+//         stack<int> s;
+//         for (int i = n - 1; i >= 0; i--) {
+//             while (!s.empty() && nums[s.top()] > nums[i])
+//                 s.pop();
+//             v[i] = s.empty() ? n : s.top();
+//             s.push(i);
+//         }
+//     }
+
+//     int longestSubarray(vector<int>& nums) {
+//         n = nums.size();
+//         vector<int> ps(n), ns(n);
+
+//         getPs(nums, ps);
+//         getNs(nums, ns);
+
+//         int maxLen = 0, z = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] == 1) continue;
+//             z++;
+//             int len = ns[i] - ps[i] - 2;
+//             maxLen = max(maxLen, len);
+//         }
+//         if (z == 0) return n - 1; 
+//         return maxLen;
+//     }
+// };
