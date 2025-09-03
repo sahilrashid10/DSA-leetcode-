3848ce88fac1b2947ef627c5894893857b8d0e86
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-09-03 09:57:01 +0530

    Time: 0 ms (100%), Space: 28.2 MB (28.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> m;
-        for(int &num:nums){
-            m[num]++;
-        }
-        int ans;
-        int maxFreq = 0;
-        for(auto &[num, freq]:m){
-            if(freq >= maxFreq){
-                ans = num;
-                maxFreq = freq;
+        int num = nums[0], freq = 1;
+
+        for(int i=1; i<nums.size(); i++){
+
+            if(nums[i] != num) freq--;
+            else freq++;
+
+            if(freq == 0){
+                num = nums[i];
+                freq = 1;
             }
+
         }
-        return ans;
+        return num;
     }
 };
+
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int, int> m;
+//         for(int &num:nums){
+//             m[num]++;
+//         }
+//         int ans;
+//         int maxFreq = 0;
+//         for(auto &[num, freq]:m){
+//             if(freq >= maxFreq){
+//                 ans = num;
+//                 maxFreq = freq;
+//             }
+//         }
+//         return ans;
+//     }
+// };
