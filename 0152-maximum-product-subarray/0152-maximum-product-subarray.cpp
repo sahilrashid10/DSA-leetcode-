b5b9244a89d39702d32aa42ef5820573a6a9ce16
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-09-17 10:27:31 +0530

    Time: 0 ms (100%), Space: 17.8 MB (21.42%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        using ll = long long;
         int n = nums.size();
+        int suffix = 1;
+        int preffix = 1;
 
-        ll pre = 1;
-        ll suf = 1;
-        ll maxProduct = INT_MIN;
+        int maxProduct = INT_MIN;
+        int countZero = 0;
+
         for(int i=0; i<n; i++){
 
-            pre *= nums[i];
-            suf *= nums[n-i-1];
+            preffix = 1ll*preffix*nums[i];
+            suffix = 1ll*suffix*nums[n-i-1];
+            maxProduct = max({maxProduct, preffix, suffix, nums[i]});
+            if(nums[i] == 0){ 
+                preffix = 1;
+                countZero = 1;
+                continue;
+            }
+            if(nums[n-i-1] == 0){ 
+                suffix = 1;
+                countZero = 1;
+                continue;
+            }
 
-            maxProduct = max({pre, suf, maxProduct});
 
-            if(pre == 0) pre = 1;
-            if(suf == 0) suf = 1;
-
+        
         }
-        return maxProduct;
 
+        return maxProduct < 0 && countZero == 1 ? 0 : maxProduct;
     }
 };
+
+
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         int mp = INT_MIN;
+//         for(int i=0; i<nums.size(); i++){
+//             int p = 1;
+//             for(int j = i; j<nums.size(); j++){
+//                 p *= nums[j];
+//                 mp = max(mp, p);
+//             }
+//         }
+//         return mp;
+//     }
+// };
