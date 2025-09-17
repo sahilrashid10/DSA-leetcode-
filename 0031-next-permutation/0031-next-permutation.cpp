89e6e75a1c16b7c1dde10be30160aad159698c63
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-09-17 08:24:29 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.99%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,32 +2,23 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i=n-2;
-        while(i >=0 ){
-            while(i >=0 && nums[i] >= nums[i+1])
-                i--;
-            if( i >= 0){
-                int j= n-1;
-                while(nums[j] <= nums[i])
-                    j--;
-                swap(nums[i], nums[j]);
-                break;
-            }
-        }
-        if(i < 0) {
-            reverse(nums.begin(), nums.end());
-            return;
+
+        // 1. Find the first index 'i' from the right such that nums[i] < nums[i+1]
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
         }
 
+        // 2. If such an index is found, find the element just larger than nums[i] from the right
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        // 3. Reverse the suffix starting at i+1
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
-
-
-
-// class Solution {
-// public:
-//     void nextPermutation(vector<int>& nums) {
-//         next_permutation(nums.begin(), nums.end());
-//     }
-// };
