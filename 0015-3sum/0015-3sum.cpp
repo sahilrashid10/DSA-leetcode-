8997e60e1a55ff9a9793a2c67185669caab3de85
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-09-17 08:47:20 +0530

    Time: 58 ms (25.84%), Space: 35 MB (15.59%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    set<vector<int>> s;
-    void twoSum(vector<int>& nums, int k, int i, int j, vector<vector<int>> &result){
-        int target = -nums[k];
+    vector<vector<int>> result;
+    int n;
+    void check(vector<int>& nums, int k, int i, int j){
+        int target = -1 * nums[k];
 
         while(i < j){
-            int sum = nums[j] + nums[i];
+
+            int sum = nums[i] + nums[j];
             if(sum < target) i++;
             else if(sum > target) j--;
             else{
-                result.push_back({nums[k], nums[i], nums[j]});
+                vector<int> temp = {nums[k], nums[i], nums[j]};
+                result.push_back(temp);
                 i++;
                 j--;
-                while(i<j && nums[i] == nums[i-1]) i++;
-                while(i<j && nums[j] == nums[j+1]) j--;
-            } 
+
+                while(i < j && nums[i] == nums[i-1]) i++;
+                while(i < j && nums[j] == nums[j+1]) j--;
+            }
         }
     }
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<vector<int>> result;
-        for(int k=0; k<n-2; k++){
-            if(k>0 && nums[k] == nums[k-1])
-                continue;
-            twoSum(nums, k, k+1, n-1, result);
+        n = nums.size();
+        for(int k = 0; k <= n-2; k++){
+            if(k > 0 && nums[k] == nums[k-1]) continue;
+
+            check(nums, k, k+1, n-1);
         }
         return result;
     }
