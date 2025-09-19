0a0ca2f904d4cd21a7695040f3b1965f01cda34a
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-09-19 17:43:40 +0530

    Time: 0 ms (100%), Space: 12.5 MB (32.76%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
-    int n;
-    int ans;
-    void modifiedBs(vector<int>& nums, int s, int e){
-        if(n == 1) {ans = 0;return;}
-        if(nums[0]>nums[1]) {ans = 0;return;}
-        if(nums[n-1]>nums[n-2]){ans = n-1;return;}
-
-        while(s<=e){
-            int m = (s+e)/2;
-            if(nums[m]>nums[m-1] && nums[m]>nums[m+1]){
-                ans = m;
-                return;
-            }
-
-            if(nums[m]>nums[m-1]) s = m+1;
-
-            else if(nums[m]>nums[m+1]) e = m-1;
-            //choice
-            else s = m+1;
-        }
-    }
     int findPeakElement(vector<int>& nums) {
-        n = nums.size();
-        modifiedBs(nums, 1, n-2);
-        return ans;
+        int n = nums.size();
+        if(n == 1) return 0;
+
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-2] < nums[n-1]) return n-1;
+
+        int s = 1, e = n-2;
+
+        while(s <= e){
+            int m = s + (e - s)/2;
+
+            if(nums[m-1] < nums[m] && nums[m] > nums[m+1]){
+                return m;
+            }else if(nums[m-1] < nums[m]){
+                s = m + 1;
+            }else
+                e = m-1;
+        }
+        return -1;
     }
 };
+
+
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n == 1) return 0;
+
+//         for(int i = 1; i < n-1; i++) {
+//             if(nums[i-1] < nums[i] && nums[i] > nums[i+1])
+//                 return i;
+//         }
+//         return nums[0] > nums[n-1] ? 0:n-1;
+//     }
+// };
