6aaccb595b9c421d68a52658f2faf09789ca0ab2
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-09-25 09:39:45 +0530

    Time: 0 ms (100%), Space: 9.8 MB (80.89%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,39 +1,23 @@
-class Solution{
+class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums){
-        vector<vector<int>>power_set;
-        int n = nums.size();
-        for(int i=0; i<(1<<n); i++){     // (1<<n)==2^n - left shift
-        vector<int>sub_set;
-            for(int j=0; j<n; j++){
-                if(i & (1<<j))
-                    sub_set.push_back(nums[j]);
-            }
-            power_set.push_back(sub_set);
-        }
-        return power_set;
-    }
-};
+    vector<vector<int>> result;
+    vector<int> temp;
+    int n;
 
-// -------------------using recursion--------------------------------------------
-class Solution{
-public:
-    void solve(vector<vector<int>>&p, vector<int>s, vector<int>&nums, int i, int n){
-        //base case 
+    void solve(vector<int>& nums, int i){
         if(i == n){
-            p.push_back(s);
+            result.push_back(temp);
             return;
-        }
-        s.push_back(nums[i]);
-        solve(p, s, nums, i+1, n);
-        s.pop_back();
-        solve(p,s, nums, i+1, n);
+        } 
+
+        temp.push_back(nums[i]);
+        solve(nums, i+1);
+        temp.pop_back();
+        solve(nums, i+1);
     }
-    vector<vector<int>> subsets(vector<int>& nums){
-        vector<vector<int>>power_set;
-        int n = nums.size();
-        vector<int>sub_set;
-        solve(power_set, sub_set, nums, 0, n);
-        return power_set;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        n = nums.size();
+        solve(nums, 0);
+        return result;
     }
 };
