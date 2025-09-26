70c18e0bb8e4b3625b9d8eb537e3ac8930cf7289
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-09-26 19:47:30 +0530

    Time: 0 ms (100%), Space: 95.1 MB (80.52%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,99 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()) 
+        int n = nums1.size(); 
+        int m = nums2.size(); 
+
+        if(n > m){
             return findMedianSortedArrays(nums2, nums1);
-            
-        int n1 = nums1.size(), n2 = nums2.size();
-        int size = n1 + n2;
+        }
 
-        int low = 0, high = n1;
-        while (low <= high) {
+        int start = 0;
+        int end = n;
+        int N = n + m;
+    //x - nums1, y = nums2, px elemets from nums1 on left, py ...;
+        while(start <= end){
+            //elements from x list on left
+            int px = start + (end - start) / 2;
+            //elements from y on left
+            int py = (n + m + 1) / 2  - px;
 
-            int Px = low + (high - low) / 2;
-            int Py = (n1 + n2 + 1) / 2 - Px;
+        //left side
+            int x1 = (px == 0) ? INT_MIN : nums1[px-1];
+            int y1 = (py == 0) ? INT_MIN : nums2[py-1];
 
-            // left side
-            int x1 = (Px != 0) ? nums1[Px - 1] : INT_MIN;
-            int y1 = (Py != 0) ? nums2[Py - 1] : INT_MIN;
+        //right side
+            int x2 = (px == n) ? INT_MAX : nums1[px];
+            int y2 = (py == m) ? INT_MAX : nums2[py];
 
-            // right side
-            int x2 = (Px != n1) ? nums1[Px] : INT_MAX;
-            int y2 = (Py != n2) ? nums2[Py] : INT_MAX;
-
-            if (x1 <= y2 && y1 <= x2) {
-                if (size % 2 == 0)
-                    return (max(x1, y1) + min(x2, y2)) / 2.0;
-
-                return max(x1, y1);
+            //sorted condition
+            if(x1 <= y2 && y1 <= x2){
+                if( N % 2 == 0){
+                    return float(max(x1, y1) + (min(x2, y2)))/2;
+                }else
+                    return max(x1, y1);
             }
-
-            if (x1 <= y2) {
-                low = Px + 1;
-            } else
-                high = Px - 1;
+            if(x1 > y2)
+                end = px - 1;
+            else
+                start = px + 1;
         }
         return -1;
     }
 };
-
-// class Solution {
-// public:
-//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-// int n1 = nums1.size(), n2 = nums2.size();
-//         int med = (n1 + n2)/2;
-//         int k = 0;
-//         int i=0, j=0;
-//         int len1=-1, len2=-1;
-
-//         while(i<n1 && j<n2){
-//             if(nums1[i] <= nums2[j]){
-//                 k++;
-
-//                 if(k == med)
-//                     len2 = nums1[i];
-
-//                 if(k == med+1)
-//                     len1 = nums1[i];
-//                 i++;
-
-//             }else{
-//                 k++;
-
-//                 if(k == med)
-//                     len2 = nums2[j];
-
-//                 if(k == med+1)
-//                     len1 = nums2[j];
-
-//                 j++;
-//             }
-//         }
-//         while(i<n1){
-//                 k++;
-
-//                 if(k == med)
-//                     len2 = nums1[i];
-
-//                 if(k == med+1)
-//                     len1 = nums1[i];
-//                 i++;
-
-//         }
-//         while(j<n2){
-//                 k++;
-
-//                 if(k == med)
-//                     len2 = nums2[j];
-
-//                 if(k == med+1)
-//                     len1 = nums2[j];
-
-//                 j++;
-//         }
-
-//         int size = n1 + n2;
-//         if(size % 2 == 0) return (len1 + len2)/ 2.0;
-//         return len1;
-//     }
-// };
