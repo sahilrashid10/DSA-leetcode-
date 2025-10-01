ff53183acd8caf14cfb56e2231a236c66f669333
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-10-01 11:45:14 +0530

    Time: 0 ms (100%), Space: 8.5 MB (32.45%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string s = "";
-        int m = word1.size();
-        int n = word2.size();
-        int i=0;
-        int j = 0;
-        while(i<m && j<n){
-            if(i == j){
-                s += word1[i];
-                i++;
-            }else{
-                s += word2[j];
-                j++;
-            }
-        }
-        while(i<m){
-            s += word1[i];
-            i++;
-        }
-        while(j<n){
-            s += word2[j];
-            j++;
-        }
-        return s;
+      string ans = "";
+      int n = word1.length();
+      int m = word2.length();
+      int i=0,j=0;
+
+      while (n>i || m>j){
+       if(i<n) ans += word1[i];
+        if (j<m)ans += word2[j];
+        i++;
+        j++;
+      }
+      return ans;
     }
 };
