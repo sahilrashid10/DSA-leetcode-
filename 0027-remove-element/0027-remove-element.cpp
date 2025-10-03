9ad5fefa3b950d2a43a9fae7067ef83a381f4fe1
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-10-03 21:59:39 +0530

    Time: 0 ms (100%), Space: 11.7 MB (46.71%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int count = 0;
         int n = nums.size();
+        int k=0;
         for(int i=0; i<n; i++){
-            if(val == nums[i]){
-                count++;
-                nums[i] = 50;
-            }
+            if(nums[i] != val)
+                nums[k++] = nums[i];
         }
-        sort(nums.begin(), nums.end());
-        return n - count;
+        return k;
     }
 };
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         int count = 0;
+//         int n = nums.size();
+//         for(int i=0; i<n; i++){
+//             if(val == nums[i]){
+//                 count++;
+//                 nums[i] = 50;
+//             }
+//         }
+//         sort(nums.begin(), nums.end());
+//         return n - count;
+//     }
+// };
