8d1b51f7e4d5e4b49cec2259f5215dd2f0725dba
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-10-05 17:04:46 +0530

    Time: 47 ms (14.29%), Space: 45.2 MB (14.29%) - LeetHub

--- 4019-remove-k-balanced-substrings/4019-remove-k-balanced-substrings.cpp
+++ 4019-remove-k-balanced-substrings/4019-remove-k-balanced-substrings.cpp
@@ -2,42 +2,82 @@ class Solution {
 public:
     string removeSubstring(string str, int k) {
         int n = str.size();
-
-        stack<char> s;
+        vector<pair<char, int>> s;
         
         for(auto &c:str){
-            s.push(c);
 
-            if(s.size() >= 2*k){
-                stack<char> temp;
-                int x = k;
-                while(!s.empty() && s.top() == ')' && x>0){
-                    temp.push(s.top());
-                    s.pop();
-                    x--;
-                }
-                if(temp.size() == k){
-                    int x = k;
-                    while(!s.empty() && s.top() == '(' && x > 0){
-                        temp.push(s.top());
-                        s.pop();
-                        x--;
-                    }        
-                }
-                if(temp.size() != 2*k){
-                    while(!temp.empty()){
-                        s.push(temp.top());
-                        temp.pop();
-                    }
-                }
+            if(s.empty() || s.back().first != c){
+                s.emplace_back(c, 1);
+            }else
+                s.back().second++;
+            
+            while(s.size() > 1 && s.back().first == ')' &&
+                    s[s.size() - 2].first == '('){
+                int countClose = s.back().second;
+                int countOpen = s[s.size() - 2].second;
+
+                int minRemoval = min(countClose/k, countOpen/k);
+                if(minRemoval == 0) break;
+
+                s.pop_back();
+                s.pop_back();
+
+                countOpen -= minRemoval*k;
+                countClose -= minRemoval*k;
+
+                if(countOpen > 0) s.emplace_back('(', countOpen);
+                if(countClose > 0) s.emplace_back(')', countClose);
             }
         }
-        string ans = "";
-        while(!s.empty()){
-            ans += s.top();
-            s.pop();
+        string ans;
+        for(auto &[c, f]:s){
+            ans.append(f, c);
         }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     string removeSubstring(string str, int k) {
+//         int n = str.size();
+
+//         stack<char> s;
+        
+//         for(auto &c:str){
+//             s.push(c);
+
+//             if(s.size() >= 2*k){
+//                 stack<char> temp;
+//                 int x = k;
+//                 while(!s.empty() && s.top() == ')' && x>0){
+//                     temp.push(s.top());
+//                     s.pop();
+//                     x--;
+//                 }
+//                 if(temp.size() == k){
+//                     int x = k;
+//                     while(!s.empty() && s.top() == '(' && x > 0){
+//                         temp.push(s.top());
+//                         s.pop();
+//                         x--;
+//                     }        
+//                 }
+//                 if(temp.size() != 2*k){
+//                     while(!temp.empty()){
+//                         s.push(temp.top());
+//                         temp.pop();
+//                     }
+//                 }
+//             }
+//         }
+//         string ans = "";
+//         while(!s.empty()){
+//             ans += s.top();
+//             s.pop();
+//         }
+//         reverse(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
