568850c516935303160970bc6d87839191c7b7cc
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-10-06 19:07:39 +0530

    Time: 0 ms (100%), Space: 112.6 MB (19.06%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,23 +1,61 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int total = 0;     // overall gas - cost balance
-        int tank = 0;      // current fuel in tank
-        int start = 0;     // candidate start index
         int n = gas.size();
+        int canTravel = accumulate(gas.begin(), gas.end(), 0) -
+        accumulate(cost.begin(), cost.end(), 0);
+        if(canTravel < 0) return -1;
 
-        for (int i = 0; i < n; i++) {
-            int diff = gas[i] - cost[i];
-            total += diff;
-            tank += diff;
+        int s = 0;
+        int cg = 0;
+        for(int i=0; i<n; i++){
+            cg += gas[i] - cost[i];
+            if(cg < 0){
+                s = (i+1) % n;
+                cg = 0;
+            }
+        }
+        return s;
+    }
+};
 
-            // if tank goes negative, we cannot reach station i+1 from current start
-            if (tank < 0) {
-                start = i + 1;  // reset start to next station
-                tank = 0;       // reset tank
+/*
+starting greedily at an index doesn't work
+gas =
+[5,8,2,8]
+cost =
+[6,5,6,6]
+
+Use Testcase
+Output
+-1
+Expected
+3
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int s = 0;
+        int n = gas.size();
+        int maxVal = INT_MIN;
+        for(int i=0; i<n; i++){
+            if(gas[i] - cost[i] > maxVal){
+                s = i;
+                maxVal = gas[i] - cost[i];
             }
         }
 
-        return total >= 0 ? start : -1;
+        int i=s;
+        int ng = 0;
+        int flag = 1;
+        while(i != s || flag){
+            flag = 0;
+            int cg = gas[i];
+            ng += cg - cost[i];
+            if(ng < 0) return -1;
+            i = (i+1) % n;
+        }
+        return s == i? s:-1;
     }
 };
+*/
