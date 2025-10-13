f6f1e4fcf7cf299577545ddbca1d2f1ccfbfbc68
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-10-13 19:30:15 +0530

    Time: 0 ms (100%), Space: 9.2 MB (6.57%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -3,13 +3,36 @@ public:
     bool isSubsequence(string s, string t) {
         int n = s.size();
         int m = t.size();
+        unordered_map<int, vector<int>> mp;
+        for(int i=0; i<m; i++)
+            mp[t[i]].push_back(i);
+        
+        long long p = -1;
+        for(int i=0; i<n; i++){
+            if(mp.count(s[i]) == 0)
+                return false;
+            vector<int> &v = mp[s[i]];
 
-        int k = 0;
-        int i=0;
-        while(k < m){
-            if(s[i] == t[k]) i++;
-            k++;
+            auto it = upper_bound(v.begin(), v.end(), p);
+            if(it == mp[s[i]].end()) return false;
+            p = *it;
         }
-        return i == n;
+        
+        return true;
     }
 };
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         int n = s.size();
+//         int m = t.size();
+
+//         int k = 0;
+//         int i=0;
+//         while(k < m){
+//             if(s[i] == t[k]) i++;
+//             k++;
+//         }
+//         return i == n;
+//     }
+// };
