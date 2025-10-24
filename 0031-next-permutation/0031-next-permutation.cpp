ccdfb990131ac60d50e2866ef0b99f21f44ebb5d
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2025-10-24 20:05:12 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.66%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,25 +2,50 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return;
+        if (n <= 1) return;
 
-        for(int i = n-2; i>=0; i--){
-            if(nums[i] < nums[i+1]){
-            auto it = upper_bound(nums.rbegin(), nums.rbegin() + (n - i - 1), nums[i]);
-            
-            // Convert reverse_iterator to normal iterator
-            int j = n - 1 - (it - nums.rbegin());
-                swap(nums[i], nums[j]);
-                sort(nums.begin() + i + 1, nums.end());
-                return;
-            }
+        int i = n - 2;
+        // Step 1: Find first decreasing element from right
+        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+
+        if (i >= 0) {
+            // Step 2: Find the element just larger than nums[i]
+            int j = n - 1;
+            while (nums[j] <= nums[i]) j--;
+
+            swap(nums[i], nums[j]);
         }
 
-        sort(nums.begin(), nums.end());
-        return;
-
+        // Step 3: Reverse the suffix
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
+
+
+
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n == 1) return;
+
+//         for(int i = n-2; i>=0; i--){
+//             if(nums[i] < nums[i+1]){
+//             auto it = upper_bound(nums.rbegin(), nums.rbegin() + (n - i - 1), nums[i]);
+            
+//             // Convert reverse_iterator to normal iterator
+//             int j = n - 1 - (it - nums.rbegin());
+//                 swap(nums[i], nums[j]);
+//                 reverse(nums.begin() + i + 1, nums.end());
+//                 return;
+//             }
+//         }
+
+//         reverse(nums.begin(), nums.end());
+//         return;
+
+//     }
+// };
 
 
 //Solution 1:  Generate all and if size full then return sorted version
